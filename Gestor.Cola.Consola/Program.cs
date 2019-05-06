ffb746179fc285346e62973daa8a1f26de622050
Xavier Castanyer <xcastanyer@gmail.com>
2019-05-06 11:29:17 +0200

    Refactorización Gestor colas

--- Gestor.Cola.Consola/Program.cs
+++ Gestor.Cola.Consola/Program.cs
@@ -1,11 +1,4 @@
-﻿using Gestor.DAL.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gestor.Cola.Consola
+﻿namespace Gestor.Cola.Consola
 {
     class Program
     {
--- Gestor.Cola.Servicio/Servicio.cs
+++ Gestor.Cola.Servicio/Servicio.cs
@@ -1,13 +1,4 @@
-﻿using Gestor.DAL.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace Gestor.Cola.Servicio
 {
